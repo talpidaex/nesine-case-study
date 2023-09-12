3e8f88ea9b24d3186c09df1c34e98c649b801896
talpidaex <bayrakogzhn@gmail.com>
2023-09-12 22:42:42 +0300

    custom 'create-react-app' application with webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.scss";
+
+export default function App() {
+  return (
+    <div>
+        <div className="test"> Bayrak! quqeqweqwqeq</div>
+      <h1>React boiler-plate Code with webpack config.</h1>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+require("file-loader?name=[name].[ext]!./index.html");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
